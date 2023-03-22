87922f1a5b81bf33e0f0d6474da5caddf5db3fc0
kleos <36254493+amlitio@users.noreply.github.com>
2023-03-22 06:47:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function submitQuestion() {
-    var userinput = document.getElementById("userinput").value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/chat");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onload = function() {
-        var response = JSON.parse(xhr.responseText);
-        var chatlog = document.getElementById("chatlog");
-        chatlog.innerHTML += "<
+const chatForm = document.querySelector('#chat-form');
+const chatBox = document.querySelector('#chat-box');
+
+chatForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('#user-input').value.trim();
+    if (userInput === '') {
+        return;
+    }
+    const data = {
+        user_input: userInput
+    };
+    const response = await fetch('/get_response', {
+        method: 'POST',
+        body: new URLSearchParams(data)
+    });
+    const json = await response.json();
+    appendBotResponse(json.response);
+    chatBox.scrollTop = chatBox.scrollHeight;
+    chatForm.reset();
+});
+
+function appendBotResponse(response) {
+    const responseElement = document.createElement('div');
+    responseElement.classList.add('bot-response');
+    responseElement.innerHTML = `<span class="bot-bubble">${response}</span>`;
+    chatBox.appendChild(responseElement);
+}
